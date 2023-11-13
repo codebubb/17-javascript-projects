835497d83f766626786af50148d85b4a3707d549
James Bubb <jpbubb82@gmail.com>
2023-11-13 11:26:49 +0000

    chore: set starter files

--- 01-welcome/script.js
+++
@@ -1,7 +0,0 @@
-const btnElement = document.querySelector('button');
-const spanElement = document.querySelector('h1 > span');
-
-btnElement.onclick = function() {
-    const yourName = prompt('Please enter your name: ');
-    spanElement.textContent = yourName;
-}
