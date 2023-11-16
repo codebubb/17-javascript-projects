8a0a9f8a833689cae1b3c25bcd13bd0161317f80
James Bubb <jpbubb82@gmail.com>
2023-11-16 09:58:06 +0000

    feat: add table data to 11

---
+++ 11-sortable-table/script.js
@@ -0,0 +1,74 @@
+const data = [
+    {
+        id: 1,
+        title: 'iPhone 9',
+        price: 549,
+        inStock: true,
+        category: 'smartphones',
+    },
+    {
+        id: 2,
+        title: 'iPhone X',
+        price: 899,
+        inStock: true,
+        category: 'smartphones',
+    },
+    {
+
+        id: 3,
+        title: 'Samsung Universe 9',
+        price: 1249,
+        inStock: true,
+        category: 'smartphones',
+    },
+    {
+        id: 4,
+        title: 'Huawei P30',
+        price: 499,
+        inStock: false,
+        category: 'smartphones',
+    },
+    {
+        id: 5,
+        title: 'OPPOF19',
+        price: 280,
+        inStock: false,
+        category: 'smartphones',
+    },
+    {
+        id: 6,
+        title: 'MacBook Pro',
+        price: 1749,
+        inStock: true,
+        category: 'laptops',
+    },
+    {
+        id: 7,
+        title: 'Samsung Galaxy Book',
+        price: 1499,
+        inStock: false,
+        category: 'laptops'
+    },
+    {
+        id: 8,
+        title: 'Microsoft Surface Laptop 4',
+        price: 1499,
+        inStock: false,
+        category: 'laptops',
+    },
+    {
+        id: 9,
+        title: 'HP Pavilion 15-DK1056WM',
+        price: 1099,
+        inStock: true,
+        category: 'laptops',
+    },
+    {
+
+        id: 10,
+        title: 'Infinix INBOOK',
+        price: 1099,
+        inStock: true,
+        category: 'laptops',
+    }
+];
